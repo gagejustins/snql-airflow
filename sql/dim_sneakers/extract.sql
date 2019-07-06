3a13f1f5c663d6545a86417a36d911aec316ee6e
Justin Gage <gagejustins@gmail.com>
2019-07-06 23:28:21 +0000

    Adjusted to current view

--- sql/dim_sneakers/extract.sql
+++ sql/dim_sneakers/extract.sql
@@ -5,7 +5,7 @@ with events as (
 		sum(case when event_type = 'clean' then 1 else 0 end) as count_cleans,
 		sum(case when event_type = 'walk' then 1 else 0 end) as count_walks
 	from sneaker_events
-	where event_time <= DATE('{{ ds }}')
+	/* where event_time <= DATE('{{ ds }}') */
 	group by 1
 )
 select 
@@ -13,7 +13,7 @@ select
 	s.sneaker_name,
 	s.color,
 	s.created_at,
-	case when (s.sold_at is null and s.trashed_at is null and s.given_at is null) then TRUE else FALSE end as is_owned,
+	case when coalesce(s.sold_at, s.trashed_at, s.given_at) is null then TRUE else FALSE end as is_owned,
 	s.sold_at,
 	s.trashed_at,
 	s.given_at,
@@ -23,8 +23,8 @@ select
 	e.count_cleans,
 	e.count_walks,
 	DATE('{{ ds }}') as updated_at,
-	TRUE as is_current
+	/* TRUE as is_current */
 from sneakers s
 left join manufacturers m on s.manufacturer_id = m.id
 left join events e on s.id = e.sneaker_id
-where s.created_at <= DATE('{{ ds }}')
+/* where s.created_at <= DATE('{{ ds }}') */
